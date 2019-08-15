0f5a4873d882cf0de7c3002531940b92c10ccb58
sangwoo <sangwoo89118@gmail.com>
2019-08-14 18:04:38 -0700

    Requirement #1 - pseudo code

--- Program.cs
+++ Program.cs
@@ -10,6 +10,25 @@ namespace c_sharp_console_calculator
     {
         static void Main(string[] args)
         {
+            // Requirement #1 - pseudo code
+
+            /// decalre variables
+            // string - user input      
+            // int - output  
+
+            // display welcome message                     
+
+            // display instruction
+            
+            // store user input
+           
+            // parse user input into an array of strings for each numbers
+            
+            // loop thru array of strings 
+           
+            // display output          
+
+            // press any key to exit
         }
     }
 }
