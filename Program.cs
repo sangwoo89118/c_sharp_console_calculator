854438e882cefa97809aa367c1c3e7e334952af2
sangwoo <sangwoo89118@gmail.com>
2019-08-15 02:52:35 -0700

    Requirement #8, support multiple delimiters

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace c_sharp_console_calculator
     {
         static void Main(string[] args)
         {
-            // Requirement #7 - implementation
+            // Requirement #8 - implementation
 
             /// decalre variables
             // string - user input
@@ -24,16 +24,19 @@ namespace c_sharp_console_calculator
             // display instruction
             Console.WriteLine("This calculator only supports an Add operation given a signle formatted string");
             Console.WriteLine("Supports more than 2 numbers");
-            Console.WriteLine("Use a comma delimited format e.g. \"1,20\" will return 21");
             Console.WriteLine("Supports a newline character as an alternative delimiter e.g. \"1\\n2,3\" will return 6");
             Console.WriteLine("Invalid/Missing numbers should be converted to 0 e.g. \"\" will return 0; \"5,tytyt\" will return 5");
             Console.WriteLine("Deny negative numbers. An exception will be thrown that includes all of the negative numbers provided");
             Console.WriteLine("Ignore any number greater than 1000 e.g. \"2,1001,6\" will return 8");
-            Console.WriteLine("Support delimiters of any length");
-            Console.WriteLine("\tuse the format: \"//{delimiter}\\n{numbers}\" e.g. \"//[***]\\n111***22***33\" will return 66\n\n");
+            Console.WriteLine("Support multiple any delimiter");
+            Console.WriteLine("\tuse the format: \"{number},{number}\" e.g. \"1,20\"");
+            Console.WriteLine("\tuse the format: \"//{delimiter}\\n{numbers}\" e.g. \"//;\\n2;5\" will return 7");
+            Console.WriteLine("\tuse the format: \"//[{delimiter}]\\n{numbers}\" e.g. \"//[***]\\n11***22***33\" will return 66");
+            Console.WriteLine("\tuse the format: \"//[{delimiter}][{delimiter}]\\n{numbers}\" e.g. \"//[*][!!][rrr]\\n11rrr22*33!!44\" will return 110");
+            Console.WriteLine("\n\n");
 
             // store user input
-            Console.WriteLine("Please enter numbers with comma e.g. 1,20 or a custom delimiter of any length e.g. \"//[***]\\n111***22***33\"");
+            Console.WriteLine("Please enter numbers with comma or custome delimiter as shown in instruction above");
             userInput = Console.ReadLine();
 
             List<string> delimiterString = new List<string>(new string[] { ",", "\\n", "\n" });
@@ -41,13 +44,18 @@ namespace c_sharp_console_calculator
             // if user input custome delimiter
             if (userInput.IndexOf("//") == 0)
             {
-                //add it to delimiterString        
-                string customDelimiter = userInput.Substring(2, userInput.IndexOf("\\n") - 2).Replace("[", string.Empty).Replace("]", string.Empty);
-                delimiterString.Add(customDelimiter);
+                string customDelimiter = userInput.Substring(2, userInput.IndexOf("\\n") - 2);
+                string[] customDelimiterArray = customDelimiter.Split(']');
+
+                for (int i = 0; i < customDelimiterArray.Count(); i++)
+                {
+                    //add it to delimiterString 
+                    delimiterString.Add(customDelimiterArray[i].Replace("[", string.Empty));
+                }
             }
 
             // parse user input into an array of strings for each numbers  
-            string[] numbers = userInput.Split(delimiterString.ToArray(), StringSplitOptions.None);
+            string[] numbers = userInput.Split(delimiterString.ToArray(), StringSplitOptions.RemoveEmptyEntries);
 
             // int[] - negative numbers 
             List<int> negativeNums = new List<int>();
